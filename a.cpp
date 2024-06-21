a9a9d8b588ee2e3bc30b6245bda63ccb9303e16e
saatvik1879 <saatvik1879@gmail.com>
2024-06-21 12:39:56 +0530

    initial commmit

---
+++ a.cpp
@@ -0,0 +1,20 @@
+/**
+ *    author:  BerSerKeR07
+**/
+#include<iostream>
+#include<string>
+#include<queue>
+#include<vector>
+
+
+#define ll long long
+#define f(i,n) for(int i = 1;i <= n;i++)
+
+
+using namespace std;
+
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+}
