d943661c5f0e8aedb70f3e77637ff4b9ab900c09
saatvik1879 <saatvik1879@gmail.com>
2024-06-26 14:47:39 +0530

    added time

--- a.cpp
+++ a.cpp
@@ -6,21 +6,46 @@
 #include<queue>
 #include<vector>
 
-
+/*****MACROS*****/
 #define ll long long
 #define f(i,n) for(ll int i = 1;i <= n;i++)
 
 
+/*****NAMESPACES*****/
 using namespace std;
 
 
+/********TIME*******/
+using namespace chrono;
+#define LOCAL 0
+
+
+/*****GLOBAL VARIABLES*****/
 const int N = 1e5;ll int t,n;
 
 
+/*****SOLVE*****/
+void solve(){
+
+}
+
+
+/*****MAIN FUNCTION*****/
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(0);
 
+    #ifdef LOCAL
+		auto begin = high_resolution_clock::now();
+	#endif
+
+    
+
+    #ifdef LOCAL
+		auto end = high_resolution_clock::now();
+		cout << "Execution Time: " << (duration_cast<duration<double> >(end - begin).count())*1000 << "milliseconds" << endl;
+	#endif
+    return 0;
 }
 
 /*
