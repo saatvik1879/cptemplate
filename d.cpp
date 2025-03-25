a680850fe0d73df0653d3af06a8089455097a238
saatvik1879 <saatvik1879@gmail.com>
2025-03-25 21:46:35 +0530

    mar 2025 template

---
+++ d.cpp
@@ -0,0 +1,48 @@
+/**
+ *    author:  BerSerKeR07
+**/
+#include<iostream>
+
+/*****MACROS*****/
+#define ll long long
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+#define vi vector<ll>
+
+/*****NAMESPACES*****/
+using namespace std;
+
+
+/********TIME*******/
+// using namespace chrono;
+// #define LOCAL 0
+
+
+/*****GLOBAL VARIABLES*****/
+
+
+
+/*****SOLVE*****/
+void solve(){
+
+}
+
+
+/*****MAIN FUNCTION*****/
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    solve();
+
+    return 0;
+}
+
+/*
+ *  1. check value overflows (long long)
+ *  2. see the effect of having multiples test cases (especially using stl in global scope) 
+ *  3. O(1) -> O(log n) -> O(n) -> ...
+ *  4. check x->n, y->m
+ *  5. check const N !!!
+ *  6. check if the func always returns
+**/
